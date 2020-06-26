092e52db8e021690cb45a058471fce27ff7ffa92
JamesSchiller <schillerj78@gmail.com>
2020-06-25 23:12:28 -0400

    constants

--- constants.js
+++ constants.js
@@ -1,1 +1,6 @@
-const PDF_API = 'https://pdfsync-api-test.herokuapp.com/api/v1/pdf';
+
+// dev
+const PDF_API = 'http://localhost:3000/api/v1/pdf';
+
+// prod
+// const PDF_API = 'https://pdfsync-api-test.herokuapp.com/api/v1/pdf';
